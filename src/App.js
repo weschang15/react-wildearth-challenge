9543089003b63dff96e8d79a7088dbcb7893b2bc
Wesley Chang <weschang15@gmail.com>
2021-02-28 13:18:07 -0800

    feat: initialize shopify checkout and query for all products

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Products from "./components/Products";
+import { useDispatch } from "./hooks/useDispatch";
+import { useStore } from "./hooks/useStore";
 
 function App() {
+  const { client, products } = useStore();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    // we want to query for all products and trigger an action to store the product list into our global state
+    async function getProducts() {
+      const payload = await client.product.fetchAll();
+      dispatch({ type: "FETCH_PRODUCTS", payload });
+    }
+
+    // we need to check to see if the user has previously added products to their cart
+    // if they did, we need to retrieve the existing cart in order to provide the opportunity to checkout, otherwise, we can treat the user as a newbie
+    async function initializeCheckout() {
+      // to keep track of checkouts, we can store the checkout ID in local storage
+      const existingCheckoutId = localStorage.getItem("checkoutId");
+
+      if (existingCheckoutId) {
+        const existingCheckout = await client.checkout.fetch(
+          existingCheckoutId
+        );
+
+        dispatch({ type: "INITIALIZE_CHECKOUT", payload: existingCheckout });
+        return;
+      }
+
+      // newbie user, create a fresh checkout
+      const checkout = await client.checkout.create();
+      dispatch({ type: "INITIALIZE_CHECKOUT", payload: checkout });
+    }
+
+    async function go() {
+      await initializeCheckout();
+      await getProducts();
+    }
+
+    go();
+  }, [client.checkout, client.product, dispatch]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Products products={products} />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Products.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Products({ products }) {
+  return (
+    <div>
+      <h1>Product Listings</h1>
+    </div>
+  );
+}
+
+export default Products;
