a5949fbdefc292216bb44639e73b99a1f615dafe
Wesley Chang <weschang15@gmail.com>
2021-02-28 13:27:11 -0800

    feat: add basic products listing and link to individual product page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,39 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import Layout from "./Layout";
 
 function Products({ products }) {
   return (
-    <div>
+    <Layout>
       <h1>Product Listings</h1>
-    </div>
+      <div
+        style={{
+          display: "grid",
+          gap: "1em",
+          gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+        }}
+      >
+        {products.map((product) => (
+          <article
+            style={{
+              padding: "1em",
+              boxShadow:
+                "0 4px 14px rgb(128 128 128 / 8%), 0 3px 6px rgb(128 128 128 / 6%)",
+            }}
+          >
+            <h2>
+              <Link
+                to={{
+                  pathname: `/products/${product.handle}`,
+                }}
+              >
+                {product.title}
+              </Link>
+            </h2>
+          </article>
+        ))}
+      </div>
+    </Layout>
   );
 }
 
